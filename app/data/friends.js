3ceef7a7836cc8d14c116a3b33c66a71ec041ade
Abdel-Kader Ben-Mohamed <Abdel-Kader Ben-Mohamed>
2017-11-17 23:45:55 -0500

    first commit-completion to follow

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'John Snow',
+    profilePic: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjXsLvRksfXAhUC5CYKHWrACA0QjRwIBw&url=http%3A%2F%2Fwww.dailymail.co.uk%2Ffemail%2Farticle-3641721%2FJon-Snow-lookalike-sends-Game-Thrones-fans-frenzy.html&psig=AOvVaw0DEInIosxytYuG8PUNCrCw&ust=1511060825691835',
+    scores: [
+      "2",
+      "1",
+      "2",
+      "2",
+      "3",
+      "3",
+      "1",
+      "3",
+      "3",
+      "2"
+    ]
+  }
+]
+
+module.exports = friendList;
